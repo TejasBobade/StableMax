9e6a3110fdf8805775f279bc4ba15320b6b4452e
Tejas <tejasbobade2002@gmail.com>
2025-02-01 18:40:15 +0530

    completed header and added avatar and icons.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -40,6 +42,30 @@ export default function Home() {
         >
           Generate stunning images from text using AI models for free
         </motion.p>
+        <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0.95,
+          filter: "blur(10px)",
+        }}
+        animate = {{
+          opacity: 1,
+          scale: 1,
+          filter: "blur(0px)",
+        }}
+        transition={{
+          duration: 0.35,
+          delay: 0.7,
+        }}
+        >
+          <Link href="/create">
+            <Button className="mt-3 p-5 font-bold">
+              Start Creating
+            </Button>
+          </Link>
+        </motion.div>
+       
+        
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,47 @@
 "use client";
 
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from './ui/button'
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { BiLoaderCircle } from "react-icons/bi";
 
 const Header = () => {
+    const [initialLoading, setInitialLoading] = useState<boolean>(true);
+
+    const { data: session, status } = useSession();
+
+    useEffect(() => {
+        if (status !== "loading") {
+            setInitialLoading(false);
+        }
+    },[status, session]);
+
   return (
     <div className='fixed top-0 w-full h-[60px] bg-black border-b border-white/60 p-3 flex justify-between items-center'>
         <Link href={"/"}>
             <h2 className='font-bold text-xl'>StableMax</h2>
         </Link>
-        <div className="__menu">
-            <Button 
-            onClick={() => signIn("google")}
-            >
-                Login
-            </Button>
-        </div>
+        {initialLoading && status === "loading" ? 
+        (<BiLoaderCircle className='animate-spin'/>
+        ):
+        (
+        !session ? 
+        (<div className="__menu">
+        <Button 
+        onClick={() => signIn("google")}
+        >
+            Login
+        </Button>
+        </div>)
+        : 
+        (<Avatar>
+        <AvatarImage src={session.user?.image || ""} />
+        <AvatarFallback>CN</AvatarFallback>
+        </Avatar>)
+        )
+        }
     </div>
   )
 }
