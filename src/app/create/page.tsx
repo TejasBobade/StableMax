26ee7a1d093862d93f66484b6143db5a4fc14474
Tejas <tejasbobade2002@gmail.com>
2025-02-02 08:10:05 +0530

    Added a form and completed a forntend for input field.

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,30 +1,81 @@
 "use client";
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import React, { useState } from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-const page = () => {
-  return (
-    <div className='w-full p-3 h-dvh flex justify-start items-center pt-[72px] flex-col'>
-        <div className='w-full border border-red-500 p-3'>
-            <h1 className='text-center font-bold text-white text-4xl'>Create</h1>
-            <p className='text-white/60 text-center'>
-            Generate Stunning Images from Text for FREE
-            </p>
-        </div>
-        <div className='flex border border-orange-500 w-full gap-3 h-full'>
-            <div className="__form flex-[2] gap-2 flex justify-center items-start flex-col">
-                <p className='text-left text-sm text-white/80' >Type you prompt below to create any image you can imagine</p>
-                <div className='flex gap-2 w-full'>
-                    <Input placeholder='a cat sitting over a sofa...' className='max-w-[70%] transition-all'/>
-                    <Button>Generate</Button>
-                </div>
-            </div>
-            <div className="__output flex-[1] bg-white/5 rounded-lg"></div>
-        </div>
+const formSchema = z.object({
+  prompt: z.string().min(7,{message: "Prompt must be at least 7 characters long"}),
+})
+
+const Page = () => {
+  const [outputImg, setOutputImg] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    }
+  });
+
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  };
+
+
+return (
+  <div className='w-full p-3 h-dvh flex justify-start items-center pt-[72px] flex-col'>
+    <div className='w-full border border-red-500 p-3'>
+      <h1 className='text-center font-bold text-white text-4xl'>Create</h1>
+      <p className='text-white/60 text-center'>
+        Generate Stunning Images from Text for FREE
+      </p>
     </div>
-  )
+    <div className='flex border border-orange-500 w-full gap-3 h-full'>
+      <div className="__form flex-[2] gap-2 flex justify-center items-start flex-col">
+        <p className='text-left text-sm text-white/80' >Type you prompt below to create any image you can imagine</p>
+        <div className='flex gap-2 w-full'>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full flex gap-2">
+              <FormField
+                control={form.control}
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="w-full max-w-[70%]">
+                    <FormControl>
+                      <Input placeholder='a cat sitting over a sofa...' className='w-full transition-all border-white' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Generate</Button>
+            </form>
+          </Form>
+
+        </div>
+      </div>
+      <div className="__output flex-[1] bg-white/5 rounded-lg"></div>
+    </div>
+  </div>
+)
 }
 
-export default page
+export default Page
